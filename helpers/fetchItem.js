544501c6f9ddec0e06c33d5c36efc04255007d3a
Bruno Roja <brunojlee@hotmail.com>
2022-02-03 17:01:50 -0400

    Requisito 9 finalizado

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,5 +1,11 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (produto) => {
+  try {
+    const response = await fetch(`https://api.mercadolibre.com/items/${produto}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return 'You must provide an url';
+  }
 };
 
 if (typeof module !== 'undefined') {
