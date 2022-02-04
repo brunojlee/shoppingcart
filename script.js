bb13482df31d43c61c949201a0d9a232591a282b
Bruno Roja <brunojlee@hotmail.com>
2022-02-03 22:40:51 -0400

    Requisito 4 finalizado agora vai

--- script.js
+++ script.js
@@ -27,19 +27,11 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener() {
-  this.remove();
-}
-
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;  
+  li.setAttribute('onclick', 'this.remove(); saveCartItems();');
   return li;
 }
 
@@ -62,6 +54,6 @@ const appendCart = async (idProduto) => {
 };
 
 window.onload = async () => {
-  getSavedCartItems();
+  await getSavedCartItems();
   await show();
 };
